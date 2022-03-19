d46e3565a85193489a092ca08ba0cb4ce84ad6bd
worashf <uppert83@gmail.com>
2022-03-19 08:08:34 +0300

    added ant design button practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import  'antd/dist/antd.css'
+import {Button} from 'antd'
+import React ,{useState} from "react"
 
 function App() {
+  const[loading,setLoading] = useState(false)
+
+  const handleClick = e=>{
+    console.log("button clicked");
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false)
+    },20000)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type='primary' block>Primary button</Button>
+      <Button type='primary' size='large' shape ="round" style={{margin:"20px 0"}}>primary</Button>
+      <Button type='dashed' style={{margin:"0 10px",background:"grey"}}>Dashed</Button>
+      <Button type='link' ><a href='www.google.com' target="_blank">go</a></Button>
+      <Button type='default' size='small' style={{margin:"0 10px",background:"grey"}}>small</Button>
+      <Button type='text' size='large' style={{margin:"0 10px",background:"grey"}}>text</Button>
+      <Button type='primary' onClick={handleClick}> handleClick</Button>
     </div>
   );
 }
